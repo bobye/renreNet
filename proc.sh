c12dc7d1c951337643deef1b6f453c2a1612b6ad
bobye <yelpoo@gmail.com>
2012-04-20 05:04:28 +0800

    simplify proc.sh

--- proc.sh
+++ proc.sh
@@ -3,11 +3,9 @@
 echo "source,target" > cache/tmp$1.edges
 for file in cache/friendsrelation$1-*.json
 do
-cat $file| sed 's/},{/\n/g'| sed 's/\[{//g' | sed 's/}\]//g'>  cache/tmp$1.txt
-cat cache/tmp$1.txt | sed -n 's/"are_friends":1,"uid2":\([0-9]*\),"uid1":\([0-9]*\)/\1,\2/p' >> cache/tmp$1.edges
+cat $file| sed 's/},{/\n/g'| sed 's/\[{//g' | sed 's/}\]//g' | sed -n 's/"are_friends":1,"uid2":\([0-9]*\),"uid1":\([0-9]*\)/\1,\2/p' >> cache/tmp$1.edges
 done
 
-cat cache/friendsinfo$1.json| sed 's/},{/\n/g' | sed 's/\[{//g' | sed 's/}\]//g' > cache/tmp$1.txt 
 echo "uid,sex,name" > cache/tmp$1.nodes
-cat cache/tmp$1.txt | sed 's/"uid":\([0-9]*\),"sex":\([01]\),"name":"\(.*\)"/\1,\2,\3/p' -n >> cache/tmp$1.nodes
+cat cache/friendsinfo$1.json| sed 's/},{/\n/g' | sed 's/\[{//g' | sed 's/}\]//g' | sed 's/"uid":\([0-9]*\),"sex":\([01]\),"name":"\(.*\)"/\1,\2,\3/p' -n >> cache/tmp$1.nodes
 
